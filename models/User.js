5ff7bfbd0a56aa963d2f68dc1eb7eb3273d5793c
Joseph Hayes <joey.hayes.1996@gmail.com>
2023-03-27 14:16:20 -0600

    Added file structure, and began building models from the instructions reference

---
+++ models/User.js
@@ -0,0 +1,39 @@
+const { Schema, Types } = require('mongoose');
+const thoughtSchema = require('./Thought');
+
+const userSchema = new Schema(
+    {
+        username: {
+            type: String,
+            required: true,
+            unique: true,
+            // add Trim prop
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            // Validate email via mongoose matching
+        },
+        thoughts: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'Thought'
+            }
+        ],
+        friends: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'User'
+            }
+        ],
+    },
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true,
+        }
+    }
+);
+
+module.exports = userSchema;
